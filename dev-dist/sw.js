13f5be4854111ba6e40ee8ea95e4d3ae9a08a320
Gledson Cruz <gledsonlscruz@gmail.com>
2026-03-31 13:43:37 -0300

    changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.56hube8i48s"
+    "revision": "0.0d92c1q9e3c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
